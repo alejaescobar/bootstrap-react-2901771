961ef31736071eb41d663b6ed49837a21f1453ed
alejaescobar <alejandra.escobar@ieguadalupe.edu.co>
2024-12-02 14:51:40 -0500

    Add files via upload
    pagina

---
+++ Pagina taikki/script.js
@@ -0,0 +1,19 @@
+//Arreglo para guardar los empleados
+
+let empleados = [];
+
+//funcion para agregar un empleado
+
+function agregarEmpleado(nombre, puesto, salario, evaluacion){
+    let nuevoEmpleado = {
+        nombre:nombre,
+        puesto:puesto,
+        salario:salario,
+        evaluacion:evaluacion,
+    }
+}
+
+//funcion para crear la lista de empleados
+function mostrarEmpleados (){
+    
+}
